df0277992dd0cd835e8870aeac7c1d9eb4dbb89e
jiaxu <350844866@qq.com>
2021-10-09 16:52:57 +0800

    1

---
+++ src/main/java/com/zjdx/department_score_homework/DepartmentScoreHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.zjdx.department_score_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentScoreHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentScoreHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjdx/department_score_homework/DepartmentScoreHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjdx.department_score_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentScoreHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
