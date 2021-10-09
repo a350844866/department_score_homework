5b1cc7d44406b435b6af1d8b805539c3a9c29b7b
jiaxu <350844866@qq.com>
2021-10-09 17:01:24 +0800

    1

---
+++ src/main/java/com/zjdx/department_score_homework/bean/User.java
@@ -0,0 +1,18 @@
+package com.zjdx.department_score_homework.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+    /**
+     * 自增id
+     */
+    private int id;
+
+    /**
+     * 登录名
+     */
+    private String loginName;
+
+
+}
